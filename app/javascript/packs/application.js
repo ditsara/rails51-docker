900378c15196ffe4502e41f355877c6ec6f4f6a7
Dan Itsara <dan@glazziq.com>
2018-01-21 12:44:15 +0700

    sample ES6 code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,9 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+console.log('Hello World from Webpacker');
+
+import $ from 'jquery';
+$(document).ready(() => {
+  $('[data-hello]').html('Hello from JS');
+});
